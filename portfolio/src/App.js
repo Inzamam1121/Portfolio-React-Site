303946e29372e76c65ddb424f821c631d03a39c1
Inzamam Yousaf <80321391+Inzamam1121@users.noreply.github.com>
2023-08-09 19:20:20 +0500

    Navbar Created

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,7 +1,8 @@
+import NavbarD from "./Components/NavbarD"
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline bg-purple-700">
-      Hello world!
-    </h1>
+    <div>
+      <NavbarD/>
+    </div>
   )
 }
