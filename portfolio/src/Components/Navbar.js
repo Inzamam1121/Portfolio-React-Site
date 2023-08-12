eaea10760bbfd26df902fe63ec97138d9fc2eb9c
Inzamam Yousaf <80321391+Inzamam1121@users.noreply.github.com>
2023-08-12 10:24:26 +0500

    Navbar Complete

--- portfolio/src/Components/Navbar.js
+++ portfolio/src/Components/Navbar.js
@@ -1,7 +1,11 @@
-import React from "react";
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useState } from "react";
 import profileImage from "../assets/profile.jpg";
+import { HiOutlineMenu } from 'react-icons/hi';
 
 function Navbar() {
+
+  const [navOpen,setNav] = useState(false);
 
   const NavOptions = [
     {name:"Home",link:'/'},
@@ -11,8 +15,12 @@ function Navbar() {
     {name:"Contact",link:'/'},
   ]
 
+  const toggleNav = () =>{
+    setNav(!navOpen);
+  }
+
   return (
-    <div className="flex w-full bg-gray-900 justify-between p-3"> {/* Added justify-between and p-3 */}
+    <div className="flex w-full bg-gray-900 justify-between flex-wrap p-3"> {/* Added justify-between and p-3 */}
       <div className="text-white flex items-center space-x-2">
         <img
           className="w-10 h-10 rounded-full"
@@ -21,9 +29,15 @@ function Navbar() {
         />
         <span className="text-white">Inzamam Yousaf</span>
       </div>
-      <div className="flex items-center space-x-2 text-white px-3 ">
+
+      <div className="lg:hidden items-center py-3 ">
+        <HiOutlineMenu className="text-white cursor-pointer "
+        onClick={toggleNav}/>
+      </div>
+
+      <div className={`${navOpen ? "flex-col my-3 space-y-3 basis-full inline-grid items-center transition ease-linear duration-800" : "hidden"} lg:flex items-center lg:space-x-2 text-white px-3`}>
       {NavOptions.map((option, index) => (
-          <a key={index} href={option.link} className="hover:text-gray-300">
+          <a key={index} href={option.link} className={`hover:text-gray-300`}>
             {option.name}
           </a>
         ))}
